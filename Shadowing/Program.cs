2ee5de1bd62e785774168a37b658597047b1fa8b
dorukyilmazz <0dorukyilmaz0@gmail..com>
2023-12-18 12:15:27 +0300

    Gün Sonu

--- Shadowing/Program.cs
+++ Shadowing/Program.cs
@@ -5,10 +5,58 @@ global using global::System.Linq;
 global using global::System.Net.Http;
 global using global::System.Threading;
 global using global::System.Threading.Tasks;
-
-        
-namespace shadowing
+interface IHayvan
 {
-
-
+ void SesCikar();
+}
+interface IBeslen
+{
+    void Beslen();
+}
+public class Kedi: IHayvan,IBeslen
+ 
+{
+    public void SesCikar()
+    {
+        Console.WriteLine("Kedi: miyav");
+    }
+    public void Beslen()
+    {
+        Console.WriteLine("Kedi süt içti.");
+    }
+}
+public class Kopek
+: IHayvan
+, IBeslen
+{
+    public void SesCikar()
+    {
+        Console.WriteLine("Köpek: hav hav");
+    }
+    public void Beslen()
+    {
+        Console.WriteLine("Köpek et yedi");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Kedi kedi = new Kedi();
+        kedi.SesCikar();
+        kedi.Beslen();
+        Kopek kopek = new Kopek();
+        kopek.SesCikar();
+        kopek.Beslen();
+        Console.WriteLine("==================");
+        IHayvan hayvan1 = kedi;
+        IHayvan hayvan2 = kopek;
+        hayvan1.SesCikar();
+        hayvan2.SesCikar();
+        Console.WriteLine("=================");
+        IBeslen beslen1 = kedi;
+        IBeslen beslen2 = kopek;
+        beslen1.Beslen();
+        beslen2.Beslen();
+    }
 }
